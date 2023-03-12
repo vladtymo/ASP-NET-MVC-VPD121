cf5d3e10ae86f5d1e3cb97d2c869f6a7c99aed5b
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-03-12 15:11:31 +0200

    Add project files.

---
+++ Shop_MVC_VPD_121/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop_MVC_VPD_121.Models;
+using System.Diagnostics;
+
+namespace Shop_MVC_VPD_121.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // ... code ...
+            return View(); // ~/Views/Home/Index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

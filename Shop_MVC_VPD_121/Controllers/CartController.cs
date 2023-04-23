ef461aa1592f149e2d4aefbaae7d9b14f2c65802
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2023-04-23 15:20:16 +0300

    Implement cart.

---
+++ Shop_MVC_VPD_121/Controllers/CartController.cs
@@ -0,0 +1,44 @@
+﻿using Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Shop_MVC_VPD_121.Helpers;
+using System.Text.Json;
+
+namespace Shop_MVC_VPD_121.Controllers
+{
+    public class CartController : Controller
+    {
+        const string cartKey = "cartItems";
+        private readonly ShopDbContext context;
+
+        public CartController(ShopDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            // get all products in the cart
+            var ids = HttpContext.Session.Get<List<int>>(cartKey);
+            ids ??= new List<int>();
+
+            List<Product?> products = ids.Select(id => context.Products.Find(id)).ToList();
+
+            return View(products); 
+        }
+
+        public IActionResult Add(int id)
+        {
+            List<int>? ids = HttpContext.Session.Get<List<int>>(cartKey);
+
+            // if id collection is null
+            ids ??= new List<int>();
+
+            ids.Add(id);
+
+            // save product to cart
+            HttpContext.Session.Set(cartKey, ids);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Shop_MVC_VPD_121/Program.cs
+++ Shop_MVC_VPD_121/Program.cs
@@ -10,6 +10,15 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ShopDbContext>(x => x.UseSqlServer(connStr));
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,6 +35,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
